9b9d19b9eef3a2316ccade58c5258d4df5020ea2
Juwami <sean.nguyenf@yahoo.com>
2019-08-30 18:30:49 -0400

    removed navbar as I want this to be a single page application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './components/Header'
-import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
       <Header />
-      <Navbar />
     </div>
   );
 }
