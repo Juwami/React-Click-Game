67a8b1049f3c609e4714fb2b8dc84aa920a64f25
Juwami <sean.nguyenf@yahoo.com>
2019-09-01 00:25:28 -0400

    add state for current Score and High Score

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import Header from './components/Header'
-import Gameboard from './components/Scoreboard'
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Scoreboard from './components/Scoreboard';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Gameboard />
-      {/* <Buddy /> */}
-    </div>
-  );
+class App extends Component {
+  state = {
+    Score: 0,
+    highScore: 0
+  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<Scoreboard Score={this.state.Score} highScore={this.state.highScore}/>
+				{/* <Buddy /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Gameboard = (props) => (
+const Scoreboard = (props) => (
 	<div>
 		<ul class="nav nav-fill bg-dark">
-			<li class="nav-item text-white display-4">Current Score:{props.Score}</li>
-			<li class="nav-item text-white display-4">High Score:{props.highScore}</li>
+			<li class="nav-item text-white display-4">Current Score: {props.Score}</li>
+			<li class="nav-item text-white display-4">High Score: {props.highScore}</li>
 		</ul>
 	</div>
 );
 
-export default Gameboard;
+export default Scoreboard;
