71f9dce66418dd0ca4b0cb8862a57638766e4b76
Juwami <sean.nguyenf@yahoo.com>
2019-08-31 23:14:20 -0400

    remove junmbotron and reset margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="jumbotron text-center mb-0 border-bottom">
+        <div className="text-center border-bottom">
             <h1 className="display-1 font-weight-bold">
                 React-Clicky-Game
             </h1>
