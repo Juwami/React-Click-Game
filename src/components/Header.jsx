08695ff3919eaf126b108d10511ada99d7168c30
Juwami <sean.nguyenf@yahoo.com>
2019-09-01 02:04:48 -0400

    merge pulled changed into my branch and push up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function Header() {
-	return (
-		<div className="text-center border-bottom">
-			<h1 className="display-1 font-weight-bold">React-Clicky-Game</h1>
-		</div>
-	);
+    return (
+        <div className="text-center mb-0 border-bottom">
+            <h1 className="display-1 font-weight-bold">
+                React-Clicky-Game
+            </h1>
+        </div>
+    )
 }
 
 export default Header;
