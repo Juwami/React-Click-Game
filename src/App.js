7176c010d1a34946917386ee2b0c2b470d79bb06
Juwami <sean.nguyenf@yahoo.com>
2019-09-03 23:50:39 -0400

    add win alert and return if highscore does not equal 20

--- src/App.js
+++ src/App.js
@@ -22,19 +22,51 @@ class App extends Component {
 		return Starlet;
 	};
 
+	newGame = () => {
+		this.setState({
+			Score: 0,
+			Starlet: this.shuffleArray(this.state.Starlet),
+			clickArr: [],
+		});
+	};
+
+	checkWin = () => {
+		if (this.state.highScore === 20) {
+			alert("You Win!");
+			window.reload();
+		}
+		else {
+			console.log("Return")
+			return;
+		}
+	}
+
 	componentDidMount() {
 		this.setState({ Starlet: this.shuffleArray(this.state.Starlet) });
 	}
 
 	imagedClicked = (id) => {
-		console.log(this.state.clickArr.find(x => x === id));
-
-		this.setState({
-			clickArr: [ ...this.state.clickArr, id ],
-			Score: this.state.newScore,
-			highScore: this.state.newHighScore,
-			Starlet: this.shuffleArray(this.state.Starlet)
-		});
+		// console.log(this.state.clickArr.find((x) => x === id));
+		if (this.state.clickArr.find((x) => x === id)) {
+			this.newGame();
+		} else {
+			if (this.state.Score < this.state.highScore) {
+				this.setState({
+					clickArr: [ ...this.state.clickArr, id ],
+					Score: this.state.Score + 1,
+					Starlet: this.shuffleArray(this.state.Starlet)
+				});
+			}
+			else {
+				this.setState({
+					clickArr: [ ...this.state.clickArr, id ],
+					Score: this.state.Score + 1,
+					highScore: this.state.highScore + 1,
+					Starlet: this.shuffleArray(this.state.Starlet)
+				});
+				this.checkWin();
+			}
+		}
 	};
 	render() {
 		return (
